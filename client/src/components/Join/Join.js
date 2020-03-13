503433901cfd8fd3ab46be21c3885c5198870677
dhairyadwivedi <dhairiya96@gmail.com>
2020-03-14 01:38:56 +0530

    add style

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -11,7 +11,7 @@ const Join = () => {
                 <h1 className="heading"></h1>
                     <div><input placeholder="Name" className="joinInput" type="text" onChange={event => setName(event.target.value)} /></div>
                     <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={event => setRoom(event.target.value}) /></div>
-                <Link>
+                <Link onClick={ (!name || !room) ? event.preventDefault() : null } to={`/chat?name=${name}&room=${room}`}> 
                     <button className="button mt-20">Join</button>
                 </Link>
             </div>
