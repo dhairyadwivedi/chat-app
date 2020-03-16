eb18b8283ed2ea3f416917ef9c3bc540012d29f0
dhairyadwivedi <dhairiya96@gmail.com>
2020-03-17 02:38:28 +0530

    Update users.js

--- server/users.js
+++ server/users.js
@@ -1,34 +1,34 @@
 const users = [];
 
 const addUser = ({ id, name, room }) => {
-    name = name.trim().toLowerCase();
-    room = room.trim().toLowerCase();
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
 
-    const existingUser = users.find(user => user.room === room && user.name === name)
+  const existingUser = users.find(
+    user => user.room === room && user.name === name
+  );
 
-    if(existingUser){
-        return {error : 'Username is taken'}
-    }
+  if (existingUser) {
+    return { error: "Username is taken" };
+  }
 
-    const user = {id, name, room}
+  const user = { id, name, room };
 
-    users.push(user)
+  users.push(user);
 
-    return { user }; 
-}
+  return { user };
+};
 
 const removeUser = () => {
-    const index = users.findIndex(user => user.id === id);
+  const index = users.findIndex(user => user.id === id);
 
-    if(index != -1) {
-        return users.splice(index, 1)[0]
-    }
-}
+  if (index != -1) {
+    return users.splice(index, 1)[0];
+  }
+};
 
-const getUser = () => {
+const getUser = id => users.find(User => user.id === id);
 
-}
+const getUsersInRoom = room => users.filter(user => user.room === room);
 
-const getUsersInRoom = () => {
-
-}
+module.exports = { addUser, removeUser, getUser, getUsersInRoom }
