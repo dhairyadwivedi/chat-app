1cc9418dc2c5f7506d4db5a3315322241731ce3f
dhairyadwivedi <dhairiya96@gmail.com>
2020-03-18 03:08:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,26 @@ io.on("connection", socket => {
 
     if (error) return callback(error);
 
-    socket.join(user.room)
+    socket.emit("message", {
+      user: "admin",
+      text: `{user.name} has joined the chat`
+    });
+
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+
+    socket.join(user.room);
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
   });
 
   socket.on("disconnect", () => {
