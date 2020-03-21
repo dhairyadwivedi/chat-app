7d76c5a60c7c313d471f4544748350ed24339e29
dhairyadwivedi <dhairiya96@gmail.com>
2020-03-21 23:31:32 +0530

    add chatbox

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -50,13 +50,14 @@ const Chat = ({ location }) => {
   return (
     <div className="outerContainer">
       <div className="container">
-        <input
+        <InfoBar />
+        {/* <input
           value={message}
           onChange={event => setMessage(event.target.value)}
           onKeyPress={event =>
             event.key === "Enter" ? sendMessage(event) : null
           }
-        />
+        /> */}
       </div>
     </div>
   );
