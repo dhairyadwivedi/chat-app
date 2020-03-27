81c485a71838bd2eac2edf6365b3518b13c03bc4
dhairyadwivedi <dhairiya96@gmail.com>
2020-03-27 21:12:25 +0530

    Update Input.js

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-const Input = () => {
-  return <div></div>;
+import './Input.css'
+
+const Input = ({ message, setMessage, sendMessage }) => {
+  return (
+    <div>
+      <form className="form">
+        <input
+          className="input"
+          type="text"
+          placeholder="Type a message..."
+          value={message}
+          onChange={event => setMessage(event.target.value)}
+          onKeyPress={event =>
+            event.key === "Enter" ? sendMessage(event) : null
+          }
+        />
+        <button className="sendButton" onClick={event => sendMessage(event)}>Send</button>
+      </form>
+    </div>
+  );
 };
 
 export default Input;
