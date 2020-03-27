d08238999e9afea012d03443c0678fd4875fbe4c
dhairyadwivedi <dhairiya96@gmail.com>
2020-03-28 02:56:29 +0530

    Update Chat.js

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -5,6 +5,7 @@ import io from "socket.io-client";
 import InfoBar from '../InfoBar/InfoBar'
 import Input from '../Input/Input'
 import Messages from '../Messages/Messages'
+import TextContainer from '../TextContainer/TextContainer'
 import "./Chat.css";
 
 let socket;
@@ -12,6 +13,7 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [users, setUsers] = useState('');
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
 
@@ -38,6 +40,10 @@ const Chat = ({ location }) => {
     socket.on("message", message => {
       setMessages([...messages, message]);
     });
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    })
   }, [messages]);
 
   const sendMessage = (event) => {
@@ -57,6 +63,7 @@ const Chat = ({ location }) => {
         <Messages messages={messages} name={name}/>
         <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
       </div>
+      <TextContainer users={users} />
     </div>
   );
 };
